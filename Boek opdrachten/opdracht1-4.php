b572f0e6a5d5f7225108fdf21592424a3907fdad
Splashc <plopkoek250@gmail.com>
2022-09-21 14:25:57 +0200

    f

--- Boek opdrachten/opdracht1-4.php
+++ Boek opdrachten/opdracht1-4.php
@@ -1,10 +1,10 @@
 <?php
-$prijs = 100;
+$prijs = 160;
 
 if ($prijs > 150 ) {
-    echo "De prijs is boven 100 euro komt dus 19 % bij" . (prijs * 1.19);
+    echo "De prijs is boven 150 euro komt dus 19 % bij Niewue prijs:" . ($prijs * 1.19); echo "<br>";
 } else if ($prijs < 55 ) {
-1.11
+    echo "De prijs is boven 100 euro komt dus 19 % bij" . ($prijs * 1.11); echo $prijs;
 } else {
-1.16
+    echo "De prijs is boven 100 euro komt dus 19 % bij" . ($prijs * 1.16); echo $prijs;
 }
