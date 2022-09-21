12b78fe951d89babfa61f2fe7e74a63e568fc4ae
Splashc <plopkoek250@gmail.com>
2022-09-21 11:37:38 +0200

    henk

--- Boek opdrachten/opdracht1.php
+++ Boek opdrachten/opdracht1.php
@@ -1,6 +1,13 @@
 <?php 
 
 echo date("l j F Y"), "<br>";
-echo date("z");
-echo date("z")
+echo date("z"), "<br>";
+echo date("l w"), "<br>";
+echo date("F t"), "<br>";
+$lol = date("L");
+if ($lol = 0) {
+    echo date("Y"), " is geen schikkel jaar";
+} if ($lol = 1) {
+    echo date("Y"), " is wel schikkel jaar";
+}
 ?>
