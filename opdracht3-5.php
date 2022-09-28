2d6b23a26cdbd937f49bb7c23f642918c95e969c
Splashc <plopkoek250@gmail.com>
2022-09-28 13:57:13 +0200

    Update opdracht3-5.php

--- opdracht3-5.php
+++ opdracht3-5.php
@@ -1,3 +1,9 @@
-for ($i=0; $i < 11; $i++) { 
-    echo "$i x 7= " . ($i * 7) . "<br>";
+<?php
+for ($i=1   ; $i < 11; $i++) {
+    echo "Dit is de tafel van $i : ";
+    for ($a=1; $a < 11; $a++) {
+        echo($i * $a) . " ";
+        }
+    echo "<br>";
 }
+?>
